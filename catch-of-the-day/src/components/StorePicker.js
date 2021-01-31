701471450a1c5bbc1d1830b098c5b165fb46baec
Neil Coutinho <njcoutinho@gmail.com>
2021-01-31 15:04:21 +0530

    StorePicker component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class StorePicker extends React.Component {
+    
+    render() {
+        return (
+            <React.Fragment>
+                <p>title</p>
+                <p>Hello World</p>
+            </React.Fragment>
+            
+        )
+    }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,5 @@
 // let's go!
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker";
+import React from "react";
+render(<StorePicker/>, document.querySelector('#main'));
