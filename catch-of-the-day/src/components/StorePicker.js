0a2f1d67bd0861a596121b20d8290dad86e70fe4
Neil Coutinho <njcoutinho@gmail.com>
2021-02-03 15:44:27 +0530

    Form onSubmit handler

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { getFunName } from "../helpers";
 class StorePicker extends React.Component {
-    
+    onSubmit = ($event) => {
+        $event.preventDefault();
+        console.log(this);
+        console.log($event)
+    }
     render() {
         return (
             <React.Fragment>
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.onSubmit}>
                     <h2>Please enter a store</h2>
                     <input type="text" defaultValue={getFunName()}/>
                     <button type="submit">Submit</button>
