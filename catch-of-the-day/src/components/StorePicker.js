57702e709185acfdf7e338646e09fb531531d1f4
Neil Coutinho <njcoutinho@gmail.com>
2021-02-03 16:20:07 +0530

    Route using history push state

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { getFunName } from "../helpers";
+import { getFunName, slugify } from "../helpers";
 class StorePicker extends React.Component {
+
+    storeName = React.createRef();
     onSubmit = ($event) => {
         $event.preventDefault();
-        console.log(this);
-        console.log($event)
+        console.log(this.storeName.current.value)
+         const name = slugify(this.storeName.current.value);
+         this.props.history.push(`/store/${name}`);
+   
     }
     render() {
         return (
             <React.Fragment>
-                <form className="store-selector" onSubmit={this.onSubmit}>
+                <form className="store-selector" onSubmit={this.onSubmit} >
                     <h2>Please enter a store</h2>
-                    <input type="text" defaultValue={getFunName()}/>
+                    <input type="text" defaultValue={getFunName()} ref={this.storeName}/>
                     <button type="submit">Submit</button>
                 </form>
             </React.Fragment>
