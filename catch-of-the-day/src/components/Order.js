7cf6b535f02b97473a42bfb28b53e24ddec81917
Neil Coutinho <njcoutinho@gmail.com>
2021-02-09 16:19:25 +0530

    Listing order items

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,6 +1,43 @@
 import React from "react";
 import {formatPrice }from "../helpers";
 class Order extends React.Component {
+
+
+    renderOrders(fishes, orders) {
+        const items = Object.keys(orders)
+                        .map((fishKey) => {
+                            const count = this.props.orders[fishKey];
+                            const fish = this.props.fishes[fishKey];
+                           
+                            if(!fish) {
+                                return (
+                                    <li key={fishKey}>
+                                       {fishKey} fish is no longer available
+                                    </li>
+                                )
+                            }
+                            const inStock = fish.status === 'available';
+                            if(!inStock) {
+                                return (
+                                    <li key={fishKey}>
+                                       {fish.name} is no longer available
+                                    </li>
+                                )
+                            }
+                            return (
+                                
+                                <li key={fishKey}>
+                                    { count } lbs {fish.name} 
+                                    { formatPrice((count*fish.price)) }
+                                </li>
+                            )
+                        })
+        return (
+            <ul className="order">
+               {items}
+            </ul>
+        )
+    }
     render() {
 
         let total = 0;
@@ -9,11 +46,16 @@ class Order extends React.Component {
         console.log({fishes, orders})
         total = Object.keys(orders)
                     .reduce((acc, fishKey) => {
-                        const inStock = fishes[fishKey].status === 'available';
+
+                        const fish = fishes[fishKey];
+                        if(!fish) {
+                            return acc;
+                        }
+                        const inStock = fish.status === 'available';
                         console.log(inStock);
                         let count = 0;
                         if(inStock) {
-                          acc = acc + (orders[fishKey] * fishes[fishKey].price);
+                          acc = acc + (orders[fishKey] * fish.price);
                         }
 
                         return acc;
@@ -21,10 +63,13 @@ class Order extends React.Component {
                     }, 0);
         console.log({total})
 
+        const ordersList = this.renderOrders(fishes, orders);
+
         return (
             <React.Fragment>
                 <div className="order-wrap">
                     <h2>Order</h2>
+                    {ordersList}
                     <div className="total">
                         Total: <strong>{formatPrice(total)}</strong>
                     </div>
