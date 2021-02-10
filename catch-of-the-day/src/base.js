54fbddb0c0ed6a9e2483d0436a35c0212ddc7335
Neil Coutinho <njcoutinho@gmail.com>
2021-02-10 16:11:13 +0530

    Configure firebase and re-base

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,21 @@
+import Rebase from "re-base";
+import firebase from "firebase";
+
+
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBofPS1PTznKh4WAimxD3KjW9fqe7Tdnnw",
+    authDomain: "catch-of-the-day-njc.firebaseapp.com",
+    projectId: "catch-of-the-day-njc",
+    storageBucket: "catch-of-the-day-njc.appspot.com",
+    messagingSenderId: "662985616852",
+    appId: "1:662985616852:web:aabf97eea218ce6ad1fa41",
+    measurementId: "G-8GZ6453312"
+  })
+
+
+  const base = Rebase.createClass(firebaseApp.database())
+  export {firebaseApp}
+
+  export default base;
+
